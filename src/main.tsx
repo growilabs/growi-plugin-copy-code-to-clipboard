23d63166eed9bfe417e08e1cae00308dc64ea988
Yuki Takei <yuki@weseek.co.jp>
2022-10-05 15:25:17 +0000

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+declare const growiFacade: any;
+
+export const activateJstest = (): void => {
+  growiFacade.markdownRenderer.optionsGenerators.customGeneratePreviewOptions = (pagePath: string, config: any) => {
+    const options = growiFacade.markdownRenderer.optionsGenerators.generatePreviewOptions(pagePath, config);
+    options.components.code = () => 'code';
+    options.components.lsx = () => <button>lsx</button>;
+    return options;
+  };
+};
+
+(window as any).growiFacade = {
+  activateJstest,
+};
